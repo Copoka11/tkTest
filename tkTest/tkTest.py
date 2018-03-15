eb575cfccc674b20ffb780ea2ea378ac1b932c60
Copoka11 <40aaa@mail.ru>
2018-03-15 18:56:50 +0700

    123

--- tkTest/tkTest.py
+++ tkTest/tkTest.py
@@ -1,59 +1,15 @@
 from tkinter import *
-from datetime import datetime
 
-temp = 0
-after_id = ""
-
-
-def tick():
-    global temp, after_id
-    after_id = root.after(1000, tick)
-    f_temp = datetime.utcfromtimestamp(temp).strftime("%M:%S")
-    label1.configure(text=str(f_temp))
-    temp += 1
+class Question:
+    def __init__(self, main):
+        self.entry1 = Entry(main, width=3, font=15)
+        self.button1 = Button(main, text="")
 
 
 
-def start_sw():
-    btn1.grid_forget()
-    btn2.grid(row=1, columnspan=2, sticky="ew")
-    tick()
-
-
-def stop_sw():
-    btn2.grid_forget()
-    btn3.grid(row=1, column=0, sticky="ew")
-    btn4.grid(row=1, column=1, sticky="ew")
-    root.after_cancel(after_id)
-
-def continue_sw():
-    btn3.grid_forget()
-    btn4.grid_forget()
-    btn2.grid(row=1, columnspan=2, sticky="ew")
-    tick()
-
-def reset_sw():
-    global temp
-    temp = 0
-    label1.configure(text="00:00")
-    btn3.grid_forget()
-    btn4.grid_forget()
-    btn1.grid(row=1, columnspan=2, sticky="ew")
 
 
 root = Tk()
-
-root.title("Stopwatch")
-
-label1 = Label(root, width=5, font=("Tahoma", 100), text="00:00")
-label1.grid(row=0, columnspan=2)
-
-btn1 = Button(root, text="Start", font=("Tahoma", 30), command=start_sw)
-btn2 = Button(root, text="Stop", font=("Tahoma", 30), command=stop_sw)
-btn3 = Button(root, text="Continue", font=("Tahoma", 30), command=continue_sw)
-btn4 = Button(root, text="Reset", font=("Tahoma", 30), command=reset_sw)
-
-btn1.grid(row=1, columnspan=2, sticky="ew")
 
 
 root.mainloop()
