698e87b73f81d81a35a728a5757cd9ca3e6f8e98
Copoka11 <40aaa@mail.ru>
2018-04-02 16:09:03 +0700

    344

--- tkTest/test_09.py
+++ tkTest/test_09.py
@@ -5,11 +5,9 @@ class Question:
         self.entry1 = Entry(main, width=3, font=15)
         self.button1 = Button(main, text="Check It")
         self.label1 = Label(main, width=27, font=15)
-
         self.entry1.grid(row=0, column=0)
         self.button1.grid(row=0, column=1)
         self.label1.grid(row=0, column=2)
-
         self.button1.bind("<Button-1>", self.answer)
         
     def answer(self, event):
